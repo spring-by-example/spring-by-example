d96a1087cdc009833a7ea45ccf9389a5805f73de
David Winterfeldt <dwinterfeldt@springbyexample.org>
2012-08-23 11:18:17 -0700

    In progress work for first Spring Data JPA example, almost complete.

--- persistence/spring-data-jpa/src/main/java/org/springbyexample/orm/entity/Professional.java
+++ persistence/spring-data-jpa/src/main/java/org/springbyexample/orm/entity/Professional.java
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.springbyexample.orm.jpa.inheritance.bean;
+package org.springbyexample.orm.entity;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
